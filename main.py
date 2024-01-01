99597d874aa0934fb5f9d41755442846d21713ad
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 01:06:52 +0600

    Check Number of Dimensions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
-print(arr)
-
-print(type(arr))
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
