39fd48cff04ffec106dabc2842867fb58cfab9eb
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 01:18:13 +0600

    Negative Indexing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4])
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
-for i in range(len(arr)):
-    print(arr[i])
+print('Last element from 2nd dim: ', arr[1, -1])
