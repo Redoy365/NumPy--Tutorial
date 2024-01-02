ab31ead6e6403b7c078e72f30b23e6b4f594894d
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 09:45:19 +0600

    ndenumerate v1.1.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+arr = np.array([1, 2, 3])
 
-for x in np.nditer(arr[:, ::2]):
-  print(x)
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+
