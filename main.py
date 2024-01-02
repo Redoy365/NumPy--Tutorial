e186a91cc77dad9284ce2e3002d6be47ee6a36f4
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 09:08:49 +0600

    NumPy Array Copy vs View

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-arr = np.array([1, 0, 3])
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
 
-newarr = arr.astype(bool)
-
-print(newarr)
-print(newarr.dtype)
+print(arr)
+print(x)
