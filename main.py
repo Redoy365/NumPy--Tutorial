3636788d8ec84b80659c2ff992fdc634c23079d9
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 09:54:33 +0600

    array_split()

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-arr1 = np.array([1, 2, 3])
+arr = np.array([1, 2, 3, 4, 5, 6])
 
-arr2 = np.array([4, 5, 6])
+newarr = np.array_split(arr, 3)
 
-arr = np.dstack((arr1, arr2))
-
-print(arr)
+print(newarr)
