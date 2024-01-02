e10b90f4c4791032dfaf2683daba9cc78f1511cb
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 15:55:59 +0600

    Visualization of Uniform Distribution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-x = random.uniform(size=(2, 3))
+sns.distplot(random.uniform(size=1000), hist=False)
 
-print(x)
+plt.show()
