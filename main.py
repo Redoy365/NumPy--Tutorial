7040afc63c5eb8522afb910507dbbc320f06b777
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 09:58:54 +0600

    where() v1.1.1

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6])
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
 
-newarr = np.array_split(arr, 3)
+x = np.where(arr == 4)
 
-print(newarr[0])
-print(newarr[1])
-print(newarr[2])
-print(newarr)
+print(x)
