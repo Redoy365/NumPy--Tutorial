f8f24033236a4bc60e198c89684c1b9eb349d5c5
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 10:30:39 +0600

    Filter Array

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-arr = np.array([[3, 2, 4], [5, 0, 1]])
+arr = np.array([41, 42, 43, 44])
 
-print(np.sort(arr))
+x = [True, False, True, False]
+
+newarr = arr[x]
+
+print(newarr)
