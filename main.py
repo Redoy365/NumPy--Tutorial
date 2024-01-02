ac8c3922b73529532d35dd260efe361e85e24a75
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 10:31:08 +0600

    Creating the Filter Array

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import numpy as np
 
 arr = np.array([41, 42, 43, 44])
 
-x = [True, False, True, False]
+# Create an empty list
+filter_arr = []
 
-newarr = arr[x]
+# go through each element in arr
+for element in arr:
+  # if the element is higher than 42, set the value to True, otherwise False:
+  if element > 42:
+    filter_arr.append(True)
+  else:
+    filter_arr.append(False)
 
+newarr = arr[filter_arr]
+
+print(filter_arr)
 print(newarr)
