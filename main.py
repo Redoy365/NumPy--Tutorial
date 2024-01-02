4c0625b4e2ae6a5009fdae801d420174aba13a39
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-02 09:51:26 +0600

    hstack() v1.1.2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-arr1 = np.array([[1, 2], [3, 4]])
+arr1 = np.array([1, 2, 3])
 
-arr2 = np.array([[5, 6], [7, 8]])
+arr2 = np.array([4, 5, 6])
 
-arr = np.concatenate((arr1, arr2), axis=1)
+arr = np.hstack((arr1, arr2))
 
 print(arr)
