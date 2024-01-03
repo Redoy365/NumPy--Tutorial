59427f4cbeab1d1433772bec8dab4536424d0394
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 11:59:28 +0600

    setdiff1d()

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-arr1 = np.array([1, 2, 3, 4])
-arr2 = np.array([3, 4, 5, 6])
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
 
-newarr = np.intersect1d(arr1, arr2, assume_unique=True)
+newarr = np.setdiff1d(set1, set2, assume_unique=True)
 
 print(newarr)
