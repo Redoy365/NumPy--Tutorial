7218207cbe8630fa27b2c0dc68a08b5d83466379
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 11:58:38 +0600

    intersect1d()

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import numpy as np
 arr1 = np.array([1, 2, 3, 4])
 arr2 = np.array([3, 4, 5, 6])
 
-newarr = np.union1d(arr1, arr2)
+newarr = np.intersect1d(arr1, arr2, assume_unique=True)
 
 print(newarr)
