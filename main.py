3895208bd7791755c0e9c18247edc09bdf24f44a
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 09:51:10 +0600

    Visualization of Zipf Distribution

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-x = random.zipf(a=2, size=(2, 3))
+x = random.zipf(a=2, size=1000)
+sns.distplot(x[x<10], kde=False)
 
-print(x)
+plt.show()
