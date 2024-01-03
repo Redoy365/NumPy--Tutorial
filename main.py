8d957334bf04154ff271f4d2ed6c6794dbfa6876
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 09:35:40 +0600

    Visualization of Exponential Distribution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-x = random.exponential(scale=2, size=(2, 3))
+sns.distplot(random.exponential(size=1000), hist=False)
 
-print(x)
+plt.show()
