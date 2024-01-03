8997606fd34304f321e3c9a1f655d71b35743216
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 10:10:34 +0600

    Simple Arithmetic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-if type(np.add) == np.ufunc:
-  print('add is ufunc')
-else:
-  print('add is not ufunc')
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
