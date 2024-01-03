f50bffae4b11a3161f4fea2b873aaf8ecc70ad51
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 09:32:29 +0600

    Multinomial Distribution

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from numpy import random
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-sns.distplot(random.normal(scale=2, size=1000), hist=False, label='normal')
-sns.distplot(random.logistic(size=1000), hist=False, label='logistic')
+x = random.multinomial(n=6, pvals=[1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
 
-plt.show()
+print(x)
