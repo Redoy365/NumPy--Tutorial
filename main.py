195727ab6a5558e742067abe46a0dfabe288c111
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 09:27:16 +0600

    Logistic Distribution

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.uniform(size=1000), hist=False)
+sns.distplot(random.uniform(size=1000), hist=True)
 
 plt.show()
