57ebcbdf3cb2aef74e43ce68c048f86e21c737ba
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-03 11:26:21 +0600

    cumprod() Cummulative Product own function

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import numpy as np
 
 arr = np.array([5, 6, 7, 8])
 
-newarr = np.cumprod(arr)
+def compound():
+    new = []
+    new.append(arr[0])
+    for i in range(1,len(arr)):
+        new.append(arr[i]*arr[i-1])
+    print(new)
 
-print(newarr)
+compound()
